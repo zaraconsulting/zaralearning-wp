6530ade5f2a3258207c96988e501a7bb64ff0cb4
zaraconsulting <derek@zaraconsulting.org>
2021-09-13 19:54:46 -0500

    review section almost done

--- single-course.php
+++ single-course.php
@@ -98,24 +98,97 @@
                                     <?php }
                                 ?>
                             </div>
+                            <?php 
+                                $reviews = new WP_Query( 
+                                    array( 
+                                        'post_type' => 'review',
+                                        'meta_query' => array(
+                                            array(
+                                                'key' => 'review_user_id',
+                                                'compare' => 'like',
+                                                // 'value' => get_the_ID(),
+                                            )
+                                        )
+                                    )
+                                ); 
+                            ?>
                             <div id="tab4" class="tab-pane reviews fade">
                                 <div class="row">
                                     <div class="col-lg-4">
                                         <div class="avg-ratings">
-                                            <h2>4.9</h2>
+                                            <?php 
+                                                $reviews = new WP_Query( 
+                                                    array( 
+                                                        'post_type' => 'review',
+                                                        'meta_query' => array(
+                                                            array(
+                                                                'key' => 'review_user_id',
+                                                                'compare' => 'like',
+                                                                'value' => get_the_ID(),
+                                                            )
+                                                        )
+                                                    )
+                                                );
+
+                                                $ratingsList = array();
+
+                                                while( $reviews->have_posts() )
+                                                {
+                                                    $reviews->the_post();
+                                                    array_push( $ratingsList, get_field( 'review_rating' ) );
+                                                }
+                                                wp_reset_postdata();
+                                            ?>
+                                            <h2><?php echo number_format( array_sum( $ratingsList ) / count( $ratingsList ), 1 ); ?></h2>
                                             <div class="rating">
-                                                <i class="fas fa-star"></i>
-                                                <i class="fas fa-star"></i>
-                                                <i class="fas fa-star"></i>
-                                                <i class="fas fa-star"></i>
-                                                <i class="fas fa-star-half-alt"></i>
+                                                <?php 
+                                                
+                                                    $avgRatingStars = floor( array_sum( $ratingsList ) / count( $ratingsList ) );
+                                                    foreach( range( 1, $avgRatingStars ) as $a )
+                                                    { 
+                                                        ?>
+                                                            <i class="fas fa-star"></i>
+                                                        <?php
+                                                    }
+                                                    
+                                                ?>
+                                                <!-- <i class="fas fa-star-half-alt"></i> -->
                                             </div>
-                                            1,455 Ratings
+                                            <?php echo $reviews->found_posts; ?> Rating<?php echo ( $reviews->found_posts != 1 ) ? 's' : ''; ?>
                                         </div>
                                     </div>
                                     <div class="col-lg-8 rating-counter">
                                         <ul>
-                                            <li>
+                                            <?php
+
+                                                $ratingAverages = array();
+
+                                                $cleanedRatingsList = array();
+                                                foreach( range(1, 5) as $s )
+                                                {
+                                                    $ratingCount;
+                                                    $cleanedRatingsList[$s] = ( array_count_values( $ratingsList )[$s] ) ? array_count_values( $ratingsList )[$s] : 0;
+                                                }
+
+                                                foreach( array_reverse( $cleanedRatingsList, true ) as $key => $val )
+                                                {
+                                                    array_sum( $ratingsList ) / count( $ratingsList );
+                                                    ?>
+                                                        <li>
+                                                            <span><?php echo $key; ?> Star<?php echo ( $key != 1 ) ? 's' : ''; ?></span>
+                                                            <span><?php echo $val; ?></span>
+                                                            <div class="rating-bar">
+                                                                <div style="border-radius: 30px; height: 7px;" class="progress">
+                                                                    <div style="background: #ffb606; border-radius: 30px; width: <?php echo ( $val / count( $ratingsList ) )*100; ?>%" class="progress-bar" role="progressbar" aria-valuenow="<?php echo ( $val / count( $ratingsList ) ); ?>" aria-valuemin="0" aria-valuemax="<? echo count( $ratingsList ); ?>"></div>
+                                                                </div>
+                                                            </div>
+                                                        </li>            
+                                                    <?php
+
+                                                }
+
+                                            ?>
+                                            <!-- <li>
                                                 <span>5 Star</span>
                                                 <span>13</span>
                                                 <div class="rating-bar"></div>
@@ -139,7 +212,7 @@
                                                 <span>1 Star</span>
                                                 <span>0</span>
                                                 <div class="rating-bar"></div>
-                                            </li>
+                                            </li> -->
                                         </ul>
                                     </div>
                                 </div>
