573df5e97f6c7f59c605f15e0ab47f45d44a4642
zaraconsulting <derek@zaraconsulting.org>
2021-09-21 08:32:59 -0500

    fixed small screen not showing auth links

--- includes/header-navigation.php
+++ includes/header-navigation.php
@@ -9,4 +9,19 @@
         <a href="<?php echo site_url( ); ?>">Instructors</a>
     </li> -->
     <li><a href="<?php echo site_url( 'contact' ); ?>">Contact</a></li>
+    <?php
+        if( is_user_logged_in() )
+        {
+            ?>
+                <li class="small-screen-link"><a href="<?php echo wp_logout_url(); ?>">Logout</a></li>  
+            <?php
+        }
+        else
+        {
+            ?>
+                <li class="small-screen-link"><a href="<?php echo wp_login_url(); ?>">Login</a></li>  
+                <li class="small-screen-link"><a href="<?php echo wp_registration_url(); ?>">Register</a></li>  
+            <?php
+        }
+    ?>
 </ul>
